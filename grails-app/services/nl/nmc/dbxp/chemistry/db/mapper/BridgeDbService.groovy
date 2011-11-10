b0b4fe5c08e6202cac06068ba73621d339c588ac
michaelvanvliet <info@michaelvanvliet.nl>
2011-11-10 11:29:58 +0000

    Remove unused libs Better example on how to use BridgeDb

--- grails-app/services/nl/nmc/dbxp/chemistry/db/mapper/BridgeDbService.groovy
+++ grails-app/services/nl/nmc/dbxp/chemistry/db/mapper/BridgeDbService.groovy
@@ -9,28 +9,54 @@ class BridgeDbService {
 
     def example() {
 		
-		//BioDataSource.init()
+		def respString = ""
 		
-		Class.forName("org.bridgedb.file.IDMapperText");
-//		def mapper = BridgeDb.connect("idmapper-text:file:///Users/miv/Documents/workspace-sts/dbxpChemistry/dbs/compounds_demo.txt");
-//		def src = new Xref ("P07215", DataSource.getByFullName("UniProt/SwissProt Accession") );
-		def mapper = BridgeDb.connect("idmapper-text:file:///Users/miv/Documents/workspace-sts/dbxpChemistry/dbs/metabolites.txt");
-		def src = new Xref ("3333", DataSource.getByFullName("Pubchem") );
-		
-		mapper.mapID(src).each { dest ->
-				println (" >> " + dest.dump());
-		}
-		println " ..... "
-		println ""
-		
-
-//		Class.forName("org.bridgedb.webservice.bridgerest.BridgeRest")
-//		def mapper = BridgeDb.connect("idmapper-bridgerest:http://webservice.bridgedb.org/Human")
-//		def src = new Xref('17634', BioDataSource.CHEBI) // Glucose
-//		mapper.mapID(src, BioDataSource.PUBCHEM).each { dest ->
-//			println dest.getId()
-//		}
+		try {
+			Class.forName("org.bridgedb.file.IDMapperText");
+			
+			BridgeDb.connect("idmapper-text:file:///Users/miv/Documents/workspace-sts/dbxpChemistry/dbs/compounds_demo.txt").mapID(
+				new Xref ("P07215", DataSource.getByFullName("UniProt/SwissProt Accession"))).each { dest ->
+					respString += "<br />UniProt/SwissProt Accession :: P07215 >> ${dest.id} ${dest.ds} ${dest.rep}"
+			}
+			respString += "<br /> ... "
+			respString += "<br />"
 				
-		return "Done!"
+			BridgeDb.connect("idmapper-text:file:///Users/miv/Documents/workspace-sts/dbxpChemistry/dbs/metabolites.txt").mapID(
+				new Xref ("3333", DataSource.getByFullName("Pubchem"))).each { dest ->
+					respString += "<br />Pubchem :: 3333 >> ${dest.id} ${dest.ds} ${dest.rep}"
+			}
+			respString += "<br /> ... "
+			respString += "<br />"
+			
+			BridgeDb.connect("idmapper-text:file:///Users/miv/Documents/workspace-sts/dbxpChemistry/dbs/metabolites.txt").mapID(
+				new Xref ("HMDB00122", DataSource.getByFullName("HMDB"))).each { dest ->
+					respString += "<br />HMDB :: HMDB00122 >> ${dest.id} ${dest.ds} ${dest.rep}"
+			}
+			respString += "<br /> ... "
+			respString += "<br />"
+			
+			BioDataSource.init()
+			Class.forName("org.bridgedb.webservice.bridgerest.BridgeRest")
+			BridgeDb.connect("idmapper-bridgerest:http://webservice.bridgedb.org/Human").mapID(
+				new Xref('17634', BioDataSource.CHEBI), BioDataSource.PUBCHEM).each { dest ->
+					respString += "<br />Chebi :: 17634 >> Pubchem :: ${dest.id} ${dest.ds} ${dest.rep}"
+			}
+			respString += "<br /> ... "
+			respString += "<br />"
+			
+			Class.forName("org.bridgedb.webservice.bridgerest.BridgeRest")
+			BridgeDb.connect("idmapper-bridgerest:http://webservice.bridgedb.org/Human").mapID(
+				new Xref('17634', BioDataSource.CHEBI)).each { dest ->
+					respString += "<br />Chebi :: 17634 >> ${dest.id} ${dest.ds} ${dest.rep}"
+			}
+				
+			respString += "<br /> ... done! "
+			respString += "<br />"
+			
+		} catch (e) {
+			println e.dump()
+		}
+		
+		return respString
     }
 }
